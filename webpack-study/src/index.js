8c355a9b9e7691522fbf03f5ffe85c2f0d0bcaab
wenliefan <wenliefan@gmail.com>
2019-01-02 14:04:16 +0800

    rename file

--- webpack-study/src/index.js
+++ webpack-study/src/index.js
@@ -24,6 +24,21 @@ var sleep = function(){
   })
 }
 
+let {keys, values, entries} = Object;
+let obj = { a: 1, b: 2, c: 3 };
+
+for (let key of keys(obj)) {
+  console.log(key); // 'a', 'b', 'c'
+}
+
+for (let value of values(obj)) {
+  console.log(value); // 1, 2, 3
+}
+
+for (let [key, value] of entries(obj)) {
+  console.log([key, value]); // ['a', 1], ['b', 2], ['c', 3]
+}
+
 import(/* webpackChunkName: "appasync" */'./app.async.js').then(( {test} )=>{
   console.log(test())
 })
