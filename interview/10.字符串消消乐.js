5f94b11344c639c2a69a319276ba17cbac5f3cb2
fanwenlie <wenliefan@gmail.com>
2020-06-04 17:38:05 +0800

    新增: 面试题字符串消消乐

---
+++ interview/10.字符串消消乐.js
@@ -0,0 +1,54 @@
+/**
+ * 字符串消消乐。消除三个及以上相邻的字符串.
+ * 注意：bbb消除，dddd消除，ccc会相连，也需要消除
+ * input: src_str = abbbcddddccef 
+ * output: dst_str = aef
+ * 
+ * 解题思路：
+ * 1. 如果
+ * 
+ * 
+ */
+
+function happyDispel(str) {
+  const strs = str.split('')
+  let repeatCount = 1
+  let repeatStartIdx = -1
+  let repeatEndIdx = -1
+  let LoopIdx = 0
+
+  function remove(startIdx, endIdx) {
+    if (startIdx === -1 || endIdx === -1) { return }
+
+    strs.splice(startIdx, endIdx - startIdx + 1)
+
+    repeatCount = 1
+    repeatStartIdx = -1
+    repeatEndIdx = -1
+    LoopIdx = 0
+  }
+
+  for(; LoopIdx < strs.length; LoopIdx++) {
+    const curIdx = LoopIdx
+    const nextIdx = LoopIdx + 1
+    const cur = strs[curIdx]
+    const next = strs[nextIdx]
+    if (cur !== next) {
+      if (repeatCount >= 3) {
+        remove(repeatStartIdx, repeatEndIdx)
+      }
+      continue
+    }
+
+    if (repeatCount === 1) {
+      repeatStartIdx = curIdx
+    }
+    repeatCount++
+    if (repeatCount >= 3) {
+      repeatEndIdx = nextIdx
+    }
+  }
+  return strs.join('')
+}
+
+console.log(dispel('abbbcddddccefc'))
