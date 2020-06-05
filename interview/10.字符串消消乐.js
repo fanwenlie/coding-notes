07d664898fea679247ef1b7ac37b9deb77578140
fanwenlie <wenliefan@gmail.com>
2020-06-05 14:19:08 +0800

    新增: 字符串消消乐增加递归的解题方式

--- interview/10.字符串消消乐.js
+++ interview/10.字符串消消乐.js
@@ -5,11 +5,13 @@
  * output: dst_str = aef
  * 
  * 解题思路：
- * 1. 如果
- * 
- * 
+ * 1. 用repeatCount来统计是否满足字符串重复三次及以上的条件
+ * 2. repeatStartIdx和repeatEndIdx分别来保存当前重复字符串的索引
+ * 3. loopIdx：当我消除了一次字符串，使索引归0，方便再次从头开始对比
+ * 4. 如果repeatCount >= 3, 则remove, 并且重置repeatCount、repeatStartIdx和repeatEndIdx，loopIdx
  */
 
+// 用迭代的方式实现
 function happyDispel(str) {
   const strs = str.split('')
   let repeatCount = 1
@@ -51,4 +53,42 @@ function happyDispel(str) {
   return strs.join('')
 }
 
+// console.log(happyDispel('abbbcddddccefc'))
+// console.log(happyDispel('aaaaaaaax'))
+// console.log('---', happyDispel('aaaaaaaa'))
+// console.log(happyDispel('baaaabaaaaxx'))
+// console.log(happyDispel('baaaabaaaxaaaxx'))
+
+
+// 用递归的方式实现， 有bug，下面的测试用例有的无法通过
+function dispel(str) {
+  if (str.length < 3) return str
+  let total = 1
+  let baseStr = str[0]
+  
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === baseStr) {
+      total++
+      continue
+    }
+    if (total >= 3) {
+      const idx =  i - total
+      str = `${str.substring(0, idx)}${str.substring(i)}`
+      return dispel(str)
+    }
+
+    baseStr = str[i]
+    total = 1
+  }
+  if (total >= 3) {
+    let idx = str.length - total
+    str = str.substring(0, idx) + str.substring(str.length)
+  }
+  return str
+}
+
 console.log(dispel('abbbcddddccefc'))
+console.log('---', dispel('aaaaaaaax'))
+console.log('---', dispel('aaaaaaaa'))
+console.log('---', dispel('baaaabaaaaxx'))
+console.log('---', dispel('baaaabaaxaaaxx'))
