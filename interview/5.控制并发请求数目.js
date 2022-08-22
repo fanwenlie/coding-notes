7accba36db4e86ebf094489f6234af2a2fb78dbb
fanwenlie <fanwenlie@joyy.com>
2022-08-22 17:48:18 +0800

    feat: 控制并发题目增加返回值

--- interview/5.控制并发请求数目.js
+++ interview/5.控制并发请求数目.js
@@ -1,36 +1,43 @@
 /**
- * 实现并发多个请求，function request(urls, maxNumber, callback){}, 注意maxNumber, 如果一个请求提前完成了，依旧要保持maxNumber个请求
- * @param {*} urls 
- * @param {*} maxNumber 
- * @param {*} callback 
+ * 实现并发多个请求，function request(urls, maxNumber, callback){}, 注意maxNumber, 如果一个请求提前完成了，依旧要保持maxNumber个请求。并且所有请求成功后，返回值按顺序输出
+ * @param {*} urls
+ * @param {*} maxNumber
  */
 function fetchData(url) {
   return new Promise((resolve) => {
     setTimeout(() => {
       console.log(url)
-      resolve()
-    }, 1000);
+      resolve(url)
+    }, 1000)
   })
 }
-function request(urls, maxNumber, callback) {
-  const tempUrls = urls.slice(0, maxNumber)
-  const otherUrls = urls.slice(maxNumber)
-  const total = urls.length
-  let count = 0
+function request(urls, maxNumber) {
+  return new Promise((resolve, reject) => {
+    const requestUrls = urls.map((url, idx) => ({ url, idx }))
+    const tempUrls = requestUrls.slice(0, maxNumber)
+    const otherUrls = requestUrls.slice(maxNumber)
+    const results = []
+    const total = urls.length
 
-  tempUrls.forEach(url => query(url))
+    tempUrls.forEach(({ url, idx }) => query(url, idx))
 
-  function query(url) {
-    fetchData(url)
-      .then(() => {
-        count++
+    function query(url, idx) {
+      fetchData(url).then((res) => {
+        results[idx] = res
+
         if (otherUrls.length) {
-          query(otherUrls.shift())
-          return 
+          const { url: tempUrl, idx: tempIdx } = otherUrls.shift();
+          query(tempUrl, tempIdx)
         }
-        if (total === count) { callback() }
+
+        if (total === results.length) {
+          resolve(results)
+        }
       })
-  }
+    }
+  })
 }
 
-request([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3, () => { console.log('success') })
+request([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3).then((res) => {
+  console.log('success', res)
+})
