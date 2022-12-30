a35e9743ccf53f7b63105b8ba4e14686eb6dc40a
fanwenlie <fanwenlie@joyy.com>
2022-12-30 15:20:19 +0800

    feat: 新增版本号排序方法

--- test-code/19.ua.js
+++ test-code/19.ua.js
@@ -91,3 +91,28 @@ function getBrowserPlatform(userAgent) {
 
   return result
 }
+
+// 从小到大排序
+function versionSort(arr) {
+  return arr.sort((a, b) => {
+    let i = 0
+    const arr1 = a.split('.')
+    const arr2 = b.split('.')
+
+    while (true) {
+      const s1 = arr1[i]
+      const s2 = arr2[i]
+      i++
+
+      if (s1 === undefined || s2 === undefined) {
+        return arr1.length - arr2.length
+      }
+
+      if (s1 === s2) continue
+
+      return s1 - s2
+    }
+  })
+}
+
+console.log(versionSort(['2.1.0.1', '0.402.1', '10.2.1', '5.1.2', '1.0.4.5']));
